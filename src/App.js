2da75444703fdaf86ba43e26ad72531f0fed4cc7
XanderMobus <fra2_21@alu.uabcs.mx>
2024-02-22 01:00:25 -0700

    final

--- src/App.js
+++ src/App.js
@@ -4,19 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" >
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Nuevo usuario
+        </h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Usuario
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type='text'></input>
+        <p>
+          Correo
+        </p>
+        <input type='text'></input>
+        <p>
+          Contraseña
+        </p>
+        
+        <input type='text'></input>
+        <label>
+              <input type='checkbox'></input> Acepto términos y condiciones
+        </label>
+        <label>
+        <button type='submit' style={{fontSize:20}}>Cancelar</button>
+        <button type='submit' style={{fontSize:20,marginLeft:15}}>Aceptar</button>
+        </label>
       </header>
     </div>
   );
