4612c84c09eaecf6a54ffeedbcddce13c0865fd5
XanderMobus <fra2_21@alu.uabcs.mx>
2024-02-22 01:27:24 -0700

    final final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+const COLORS = {
+  white: "#EEE4E7",
+  blanco: '#FFFF',
+  black: "#3E3E3E",
+  primary: "#a65bae",
+  MoradoClaro: "#FFFFFFFF",
+  gris: '#abb2b9',
+  naranja: '#e1665a',
+}
+
+const Button = (props) => {
+  const filledBgColor = props.color || COLORS.white;
+  const outlinedColor = COLORS.blanco;
+  const bgColor = props.filled ? filledBgColor : outlinedColor;
+  const textColor = props.filled ? COLORS.black : COLORS.primary;
+
+  return (
+    <TouchableOpacity
+      style={{
+        ...stylesbutton.button,
+        ...{ backgroundColor: bgColor },
+        ...props.style,
+        opacity: props.disabled ? 0.5 : 1, 
+      }}
+      onPress={props.onPress}
+      disabled={props.disabled} 
+    >
+      <Text style={{ fontWeight: 700, fontSize: 30, color: textColor }}>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+
+const stylesbutton = StyleSheet.create({
+  button: {
+      height:(40),
+
+      borderColor: COLORS.blanco,
+      borderRadius: (10),
+      alignItems: 'center',
+      justifyContent: 'center',
+  }
+})
+
+
+
+
+
 
 function App() {
   return (
@@ -26,13 +79,23 @@ function App() {
         <label>
               <input type='checkbox'></input> Acepto términos y condiciones
         </label>
-        <label>
-        <button type='submit' style={{fontSize:20}}>Cancelar</button>
-        <button type='submit' style={{fontSize:20,marginLeft:15}}>Aceptar</button>
+        <label style={{display:'flex'}}>
+        <Button
+                    title="Cancelar"
+                    filled
+                  
+                />
+        <Button
+                    title="Iniciar Sesion"
+                    filled
+                    style={{left:10}}
+
+                />
         </label>
       </header>
     </div>
   );
 }
+
 
 export default App;
